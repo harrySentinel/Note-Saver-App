afbec096bb40054ae7282ab8cedf94ef5f58cde1
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2024-10-04 13:01:26 +0530

    initial commit

---
+++ src/redux/pasteSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  pastes:localStorage.getItem("pastes")
+  ? JSON.parse(localStorage.getItem("pastes"))
+  : []
+}
+
+export const pasteSlice = createSlice({
+  name: 'paste',
+  initialState,
+  reducers: {
+    addToPastes: (state, action) => {
+    
+    },
+    updateToPastes: (state, action) => {
+      
+    },
+    resetAllPastes: (state, action) => {
+      
+    },
+    removedFromPastes: (state,action) => {
+
+    }
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+
+export default counterSlice.reducer
