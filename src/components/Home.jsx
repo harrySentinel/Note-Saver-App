d220595648aac1a4b0f993153b2586cb2a3a2508
Aditya Srivastava <adityasrivastav9721057380@gmail.com>
2024-10-05 13:49:30 +0530

    Added basic ui to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const pasteId = searchParams.get("pasteId");
 
-export default Home
+  return (
+    <div>
+      <div className="flex flex-row gap-7 place-content-between">
+        <input
+          className="p-1 rounded-2xl border-black mt-2 w-[66%] pl-4"
+          type="text"
+          placeholder="Enter your note here"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button className="p-2 rounded-2xl border-black mt-2">
+          {pasteId ? "update My Paste" : "Create My Paste"}
+        </button>
+      </div>
+      <div className="mt-8">  
+        <textarea
+        className="rounded-2xl border-black mt-4, min-w-[500px] p-4 min-h-[400px]"
+        value = {value}
+        placeholder="Enter Content here..."
+        onChange={(e) => setValue(e.target.value)}
+        rows={20}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
